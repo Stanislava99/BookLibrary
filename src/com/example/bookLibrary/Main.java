1e28fee13bd0e29cb917cc1ba087fa32dd496b3b
Stanislava99 <stanislavam@live.co.uk>
2021-05-29 17:37:24 +0200

    final change

--- src/com/example/bookLibrary/Main.java
+++ src/com/example/bookLibrary/Main.java
@@ -14,37 +14,77 @@ public class Main {
         List<Book> books = new ArrayList<>();
         List<Author> authors = new ArrayList<>();
 
-        System.out.println("Enter first letter of your author's last name: ");
-        char c = input.next().charAt(0);
-
         authors.add(0, new Author("Sigmund", "Freud", 1856));
         authors.add(1, new Author("Immanuel", "Kant", 1724));
         authors.add(2, new Author("Hermann", "Hesse", 1877));
         authors.add(3, new Author("Dan", "Brown", 1964));
         authors.add(4, new Author("Wilson", "Harris", 1921));
 
-        books.add(0, new EBook("The Interpretation of Dreams", "48659525", 1899, authors.get(0), com.example.bookLibrary.EBook.Format.epub,150));
-        books.add(1, new EBook("Beyond The Pleasure Principle", "54851397", 1920, authors.get(0), com.example.bookLibrary.EBook.Format.html5,200));
-        books.add(2, new PrintCopy("Dream Psychology", "54985732", 1921, authors.get(0),168,1.0));
-        books.add(3, new EBook("A General Introduction to Psychoanalysis", "25483315", 1917, authors.get(0), com.example.bookLibrary.EBook.Format.html5,123));
+        books.add(0, new EBook("The Interpretation of Dreams", "48659525", 1899, authors.get(0), com.example.bookLibrary.EBook.Format.epub, 150));
+        books.add(1, new EBook("Beyond The Pleasure Principle", "54851397", 1920, authors.get(0), com.example.bookLibrary.EBook.Format.html5, 200));
+        books.add(2, new PrintCopy("Dream Psychology", "54985732", 1921, authors.get(0), 168, 1.0));
+        books.add(3, new EBook("A General Introduction to Psychoanalysis", "25483315", 1917, authors.get(0), com.example.bookLibrary.EBook.Format.html5, 123));
 
-        books.add(4, new PrintCopy("Critique of Pure Reason", "78912435", 1781, authors.get(1),213,2.8));
+        books.add(4, new PrintCopy("Critique of Pure Reason", "78912435", 1781, authors.get(1), 213, 2.8));
 
-        books.add(5, new PrintCopy("Steppenwolf", "16485975", 1927, authors.get(2),96,1.2));
+        books.add(5, new PrintCopy("Steppenwolf", "16485975", 1927, authors.get(2), 96, 1.2));
 
-        books.add(6, new EBook("Da Vinci Code", "49315785", 2003, authors.get(3), com.example.bookLibrary.EBook.Format.epub,156));
-        books.add(0, new EBook("Angels and Demons", "96584732", 2000, authors.get(3), com.example.bookLibrary.EBook.Format.pdf,300));
+        books.add(6, new EBook("Da Vinci Code", "49315785", 2003, authors.get(3), com.example.bookLibrary.EBook.Format.epub, 156));
+        books.add(0, new EBook("Angels and Demons", "96584732", 2000, authors.get(3), com.example.bookLibrary.EBook.Format.pdf, 300));
 
-        printBooks(books);
-        System.out.println("----------------------");
-        getBooksBySurname(c, books);
-        System.out.println("----------------------");
-        authorBooksDecade(authors.get(4), books);
-        System.out.println("----------------------");
-        authorsThreeBooks(authors, books);
-        System.out.println("----------------------");
-        booksOldestNewest(books);
- }
+        startText();
+        int number = 0;
+        char character;
+        while (number != 6) {
+            System.out.println("Enter a number: ");
+            number = input.nextInt();
+
+            if (number > 6 && number < 1) {
+                System.out.println("Error input, try again");
+                startText();
+                number = input.nextInt();
+            } else if (number == 1) {
+                System.out.println("----------------------");
+                printBooks(books);
+                System.out.println("----------------------");
+
+
+            } else if (number == 2) {
+                System.out.println("Enter first letter of author's last name");
+                character = input.next().charAt(0);
+                System.out.println("----------------------");
+                getBooksBySurname(character, books);
+                System.out.println("----------------------");
+
+            } else if (number == 3) {
+                System.out.println("----------------------");
+                authorBooksDecade(authors.get(4), books);
+                System.out.println("----------------------");
+
+            } else if (number == 4) {
+                System.out.println("----------------------");
+                authorsThreeBooks(authors, books);
+                System.out.println("----------------------");
+
+            } else if (number == 5) {
+                System.out.println("----------------------");
+                booksOldestNewest(books);
+                System.out.println("----------------------");
+
+            }
+        }
+        System.out.println("End!");
+    }
+
+    public static void startText() {
+        System.out.println("Welcome, choose what you want to do(with number) \n" +
+                "1 - Print all the books in increasing (year) order \n" +
+                "2 - Prints all the books whose author's last name starts with entered character \n" +
+                "3 - Prints all the books that are written in the authors' decade \n" +
+                "4 - Prints authors that have more than 3 books \n" +
+                "5 - Prints the oldest and the newest book added \n" +
+                "6 - Exit");
+    }
 
     //Prints all the books in increasing (year) order
     public static void printBooks(List<Book> books) {
@@ -97,11 +137,12 @@ public class Main {
             }
             if (count > 3) {
                 System.out.println("Authors with more than 3 books: ");
-                System.out.print(author);
+                System.out.println(author);
             }
         }
     }
 
+    //Prints the oldest and the newest book added.
     public static void booksOldestNewest(List<Book> books) {
         List<Book> sortedBooks = books.stream()
                 .sorted(Comparator.comparing(Book::getYear))
@@ -109,5 +150,6 @@ public class Main {
         System.out.println("Oldest book: ");
         System.out.println(sortedBooks.get(0).toString());
         System.out.println("Newest book: ");
-        System.out.println(books.get(sortedBooks.size() - 1).toString());    }
+        System.out.println(books.get(sortedBooks.size() - 1).toString());
+    }
 }
